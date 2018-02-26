888342bed7ed1f238ccc5b41390150d2b6954f89
Van Do <VenusDo@Vans-MBP.gsu.edu>
2018-02-26 12:59:21 -0500

    added style for navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,39 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        $(document).ready(function() {
+            if ($('.navbar>ul>li').hasClass('selected')){
+                $('.selected').addClass('active');
+                var currentleft = $('.selected').position().left + "px";
+                var currentwidth = $('.selected').css('width');
+                $('.pointer').css({"left": currentleft,"width": currentwidth});
+            } else {
+                $('.navbar>ul>li').first().addClass('active');
+                var currentleft = $('.active').position().left + "px";
+                var currentwidth = $('.active').css('width');
+                $('.pointer').css({"left": currentleft,"width": currentwidth});
+            }
+            $('.navbar>ul>li').hover(function(){
+                $('.navbar ul li').removeClass('active');
+                $(this).addClass('active');
+                var currentleft = $('.active').position().left + "px";
+                var currentwidth = $('.active').css('width');
+                $('.pointer').css({"left": currentleft,"width": currentwidth});
+            },function(){
+                if ($('.navbar>ul>li').hasClass('selected')){
+                    $('.selected').addClass('active');
+                    var currentleft = $('.selected').position().left + "px";
+                    var currentwidth = $('.selected').css('width');
+                    $('.pointer').css({"left": currentleft,"width": currentwidth});
+                } else {
+                    $('.navbar>ul>li').first().addClass('active');
+                    var currentleft = $('.active').position().left + "px";
+                    var currentwidth = $('.active').css('width');
+                    $('.pointer').css({"left": currentleft,"width": currentwidth});
+                }
+            });
+        });
+    }
 
 }
